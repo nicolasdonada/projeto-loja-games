227c82b6c51d826285cf782e71c562980c793b94
nicolasdonada <nicolasdonada12@gmail.com>
2025-12-09 19:24:03 -0300

    feat: finalizado a criacao dos metodos

--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Produto } from "../entities/produto.entity";
 
 @Injectable()
@@ -22,8 +22,38 @@ export class ProdutoService {
         });
 
         if (!produto)
-            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+            throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
 
         return produto;
+    }
+
+    async findByNome(nome: string): Promise<Produto[]> {
+        return await this.produtoRepository.find({
+            where: {
+                nm_produto: ILike(`%${nome}%`)
+            }
+        });
+    }
+
+    async create(produto: Produto): Promise<Produto> {
+        return await this.produtoRepository.save(produto);
+    }
+
+    async update(produto: Produto): Promise<Produto> {
+        let buscaProduto = await this.findById(produto.id_produto);
+
+        if (!buscaProduto || !produto.id_produto)
+            throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+
+        return await this.produtoRepository.save(produto);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        let buscaProduto = await this.findById(id);
+
+        if (!buscaProduto)
+            throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+
+        return await this.produtoRepository.delete(id);
     }
 }
